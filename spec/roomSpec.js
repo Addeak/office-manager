8b467582e38a557841aa7e56bab892aea0a7921c
Adam Deakin <adeakin89@gmail.com>
2022-04-13 17:06:10 +0100

    Added availability feature

--- spec/roomSpec.js
+++ spec/roomSpec.js
@@ -6,9 +6,21 @@ describe('Room', () => {
     room = new Room('A')
   })
 
-  describe('#name', () => {
-    it('has a name', () => {
-      expect(room.name()).toEqual('A')
+  it('has a name', () => {
+    expect(room.name()).toEqual('A')
+  })
+
+  describe('when NOT in use', () => {
+    it('is available', () => {
+      expect(room.isAvailable()).toEqual(true)
     })
   })
+
+  describe('when in use', () => {
+    it('is NOT available', () => {
+      room.switchToUnavailable()
+      expect(room.isAvailable()).toEqual(false)
+    })
+  })
+
 })
--- src/room.js
+++ src/room.js
@@ -3,9 +3,18 @@
 class Room {
   constructor(name) {
     this._name = name
+    this._available = true
   }
 
   name() {
     return this._name
+  }
+
+  isAvailable() {
+    return this._available
+  }
+
+  switchToUnavailable() {
+    this._available = false
   }
 }
