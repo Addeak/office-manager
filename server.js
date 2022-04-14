c29225b01d2ebacbc7be30a756654c2f97f2b752
Adam Deakin <adeakin89@gmail.com>
2022-04-14 12:12:58 +0100

    Tried to enable user registration but so far route not working

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const connectDB = require('./db')
 const express = require('express')
 
 const app = express()
+app.use(express.json())
+app.use('/api/auth', require('./Auth/route'))
 const PORT = 5000
 const server = app.listen(PORT, () =>
   console.log(`Server connected to port ${PORT}`)
